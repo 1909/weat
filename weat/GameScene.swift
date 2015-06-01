0fa3bd3d4fda3d15170d6cb362ed6ddcf60ea7c5
Thanabordi Jianrungsang <sunsk128@gmail.com>
2015-05-31 17:45:46 -0700

    0,0 doesn't show up on the screen

--- weat/GameScene.swift
+++ weat/GameScene.swift
@@ -8,13 +8,16 @@
 
 import SpriteKit
 
-let PLATFORM = UInt32(0x1 << 0);
-let PLAYER   = UInt32(0x1 << 1);
+let PLATFORM = UInt32(0x1 << 0)
+let PLAYER   = UInt32(0x1 << 1)
+let NUM_COLUMN = 4
 
 class GameScene: SKScene {
     
     var player : SKSpriteNode?;
     var platform: SKSpriteNode?;
+    var myWorld: SKNode?;
+    
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -23,6 +26,10 @@ class GameScene: SKScene {
 //        myLabel.fontSize = 20;
 //        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
 //        self.addChild(myLabel)
+        
+        self.myWorld = SKNode();
+        self.addChild(self.myWorld!);
+        
         
         self.physicsWorld.gravity = CGVectorMake(0, -6);
         let physicsBody = SKPhysicsBody (edgeLoopFromRect: self.frame);
@@ -33,7 +40,8 @@ class GameScene: SKScene {
         player.xScale = 0.7;
         player.yScale = 0.7;
         player.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        self.addChild(player);
+        self.myWorld!.addChild(player);
+        //self.addChild(player);
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.frame.size.width/2);
         player.physicsBody!.friction = 0.3;
 //        player.physicsBody!.restitution = 0.8;
@@ -43,13 +51,26 @@ class GameScene: SKScene {
         player.physicsBody?.collisionBitMask = PLATFORM;
         self.player = player;
         
-        let platform = SKSpriteNode(imageNamed:"MossPlatform")
-        platform.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        self.addChild(platform)
-        platform.physicsBody = SKPhysicsBody(rectangleOfSize: platform.frame.size)
-        platform.physicsBody?.dynamic = false;
-        platform.physicsBody?.categoryBitMask = PLATFORM;
-        self.platform = platform
+//        let platform = SKSpriteNode(imageNamed:"MossPlatform")
+//        platform.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+//        self.myWorld!.addChild(platform)
+//        platform.physicsBody = SKPhysicsBody(rectangleOfSize: platform.frame.size)
+//        platform.physicsBody?.dynamic = false;
+//        platform.physicsBody?.categoryBitMask = PLATFORM;
+//        self.platform = platform
+        
+//        createPlatform(2, yPosition: 0);
+        createPlatform(1, yPosition: 400);
+        createPlatform(2, yPosition: 400);
+        
+        //createPlatform(2, yPosition: 400);
+        //createPlatform(2, yPosition: 450);
+        //let platform = SKSpriteNode(imageNamed:"MossPlatform")
+        //platform.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        //self.myWorld!.addChild(platform)
+        //let platform2 = SKSpriteNode(imageNamed:"MossPlatform")
+        //platform2.position = CGPointMake(384, 400)
+        //self.myWorld!.addChild(platform2)
 
     }
     
@@ -71,7 +92,22 @@ class GameScene: SKScene {
         } else {
             self.player?.physicsBody?.collisionBitMask = PLATFORM;
         }
-
-        
     }
+    
+    override func didFinishUpdate() {
+        self.myWorld?.position = CGPointMake(CGRectGetMidX(self.frame) - self.player!.position.x, CGRectGetMidY(self.frame) - self.player!.position.y)
+    }
+    
+    func createPlatform(column: Int, yPosition: CGFloat) {
+        let platform = SKSpriteNode(imageNamed:"MossPlatform")
+        let col_w   = CGFloat(self.frame.size.width) / CGFloat(NUM_COLUMN)
+        platform.position = CGPointMake(col_w * (CGFloat(column) + 0.5), yPosition)
+        print (platform.position)
+        self.myWorld!.addChild(platform)
+        //self.addChild(platform)
+        platform.physicsBody = SKPhysicsBody(rectangleOfSize: platform.frame.size)
+        platform.physicsBody?.dynamic = false;
+        platform.physicsBody?.categoryBitMask = PLATFORM;
+    }
+    
 }
